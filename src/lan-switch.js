e0616f8d8156bf38491c67cbcf89ecdcda29383d
Varga-Tóth Máté <vargatothmate@gmail.com>
2024-08-10 16:37:37 +0200

    finished about section

--- src/lan-switch.js
+++ src/lan-switch.js
@@ -23,7 +23,7 @@ async function fetchLanguageData(lang) {
 function updateContent(langData) {
   document.querySelectorAll("[data-i18n]").forEach((element) => {
     const key = element.getAttribute("data-i18n");
-    element.textContent = langData[key];
+    element.innerHTML = langData[key];
   });
 }
 
